a8728e7a6f0d7ff71de2afaaec4750e0afa85351
Ishwar <110125203+Ishwar6010@users.noreply.github.com>
2023-03-05 18:01:26 +0530

    Create insertnodeinBST.cpp

---
+++ insertnodeinBST.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root)
+            return new TreeNode(val);
+        TreeNode* curr=root;
+        while(true)
+        {
+            if(curr->val<=val)
+            {
+                if(!curr->right)
+                {
+                    curr->right=new TreeNode(val);
+                    break;
+                }
+                else
+                {
+                    curr=curr->right;
+                }
+            }
+            else
+            {
+                if(!curr->left)
+                {
+                    curr->left=new TreeNode(val);
+                    break;
+                }
+                else
+                {
+                    curr=curr->left;
+                }
+            }
+        }
+        return root;
+    }
+};
